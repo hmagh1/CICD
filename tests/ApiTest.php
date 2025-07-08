792b5234969e609be4578c9a62fe81cdce6b7869
hmagh1 <129678389+hmagh1@users.noreply.github.com>
2025-07-08 09:30:42 +0100

    Update ApiTest.php

--- tests/ApiTest.php
+++ tests/ApiTest.php
@@ -1,23 +1,32 @@
-<?php
 use PHPUnit\Framework\TestCase;
 
 class ApiTest extends TestCase {
     private $baseUrl = 'http://php:8000';
+    private $authHeader;
 
-    public function testUserCrudFlow() {
-        // Clean slate
+    protected function setUp(): void {
+        $credentials = base64_encode('admin:1234');
+        $this->authHeader = 'Authorization: Basic ' . $credentials;
+
+        // Clean up users
         $ch = curl_init("{$this->baseUrl}/users");
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [$this->authHeader]);
         curl_exec($ch);
         curl_close($ch);
+    }
 
-        // Add a user
+    public function testUserCrudFlow() {
+        // Create user
         $data = json_encode(["name" => "Eve", "email" => "eve@example.com"]);
         $ch = curl_init("{$this->baseUrl}/users");
         curl_setopt($ch, CURLOPT_POST, true);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            'Content-Type: application/json',
+            $this->authHeader
+        ]);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         $result = curl_exec($ch);
         curl_close($ch);
@@ -26,6 +35,7 @@ class ApiTest extends TestCase {
         // List users
         $ch = curl_init("{$this->baseUrl}/users");
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [$this->authHeader]);
         $output = curl_exec($ch);
         curl_close($ch);
         $users = json_decode($output, true);
